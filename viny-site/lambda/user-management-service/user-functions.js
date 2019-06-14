b0bab47de0d4c299aa1325595d36689e47bdf2bc
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2019-06-14 12:02:56 +1000

    Add functionality to update user details, get user forum posts and delete user records

--- viny-site/lambda/user-management-service/user-functions.js
+++ viny-site/lambda/user-management-service/user-functions.js
@@ -1,26 +1,35 @@
 const _ = require('lodash');
 const db_util = require('../utils/db-util');
+const ObjectID = require('mongodb').ObjectID;
 
 const update_user = async (uid, userdata) => {
   const db = await db_util.connect_db();
 
-  await db.collection('users').findOneAndUpdate({uid: uid},
-    {
-      $set: {
-        family_name: userdata.family_name,
-        given_name: userdata.give_name,
-        name: userdata.name,
-        picture: '',
-        updatedAt: new Date()
-      }
-    },
+  const set_user = {
+    $set: {
+      family_name: userdata.family_name,
+      given_name: userdata.given_name,
+      name: userdata.given_name + " "+ userdata.family_name,
+      updatedAt: new Date()
+    }
+  };
+
+  if (!_.isEmpty(userdata.picture)) {
+    _.assign(set_user.$set, {
+      picture: userdata.picture
+    });
+  }
+
+  const newUserData = await db.collection('users').findOneAndUpdate({uid: uid},
+    set_user,
     {
       returnOriginal: false,
       upsert: false
     });
 
-  return true;
+  return newUserData;
 };
+
 
 const get_user = async (uid) => {
   const db = await db_util.connect_db();
@@ -28,7 +37,145 @@ const get_user = async (uid) => {
   return await db.collection('users').findOne({uid: uid});
 };
 
+
+const get_user_records = async (uid, query_params) => {
+
+  const limit = _.parseInt(_.get(query_params, 'limit', 30));
+  const skip = _.parseInt(_.get(query_params, 'skip', 0));
+
+  const db = await db_util.connect_db();
+
+  const data = await db.collection('records').aggregate([
+    {
+      $match: {
+        ownerUid: uid,
+        latest: true
+      }
+    },
+    {
+      $facet: {
+        data: [{$count: "total"}],
+        records: [
+          {
+            $skip: skip
+          },
+          {
+            $limit: limit
+          },
+          {
+            $project: {
+              name: 1,
+              label: 1,
+              genres: 1,
+              chosenImage: 1,
+              images: 1,
+              id: 1,
+              score: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {count: {$arrayElemAt: ["$data", 0]}}
+    },
+    {
+      $addFields: {
+        count: "$count.total",
+        limit: limit,
+        skip: skip
+      }
+    },
+    {
+      $project: {
+        data: 0
+      }
+    }
+  ]).toArray();
+
+  return data[0];
+
+};
+
+
+const get_user_forum_posts = async (uid, query_params) => {
+
+  const limit = _.parseInt(_.get(query_params, 'limit', 5));
+  const skip = _.parseInt(_.get(query_params, 'skip', 0));
+
+  const db = await db_util.connect_db();
+
+  const data = await db.collection('forum_posts').aggregate([
+    {
+      $match: {
+        ownerUid: uid
+      }
+    },
+    {
+      $facet: {
+        data: [{$count: "total"}],
+        posts: [
+          {
+            $skip: skip
+          },
+          {
+            $limit: limit
+          },
+          {
+            $project: {
+              postTitle: 1,
+              _id: 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {count: {$arrayElemAt: ["$data", 0]}}
+    },
+    {
+      $addFields: {
+        count: "$count.total",
+        limit: limit,
+        skip: skip
+      }
+    },
+    {
+      $project: {
+        data: 0
+      }
+    }
+  ]).toArray();
+
+  return data[0];
+
+};
+
+
+const delete_record = async (uid, recordId) => {
+
+  recordId = "5cf8ee10f780eb0007a686c9"
+
+  const db = await db_util.connect_db();
+  // const record = await db.collection('records').findOne({_id: ObjectID(recordId)});
+
+  let query = {
+    // ensure only the owner or an admin can delete
+    id: ObjectID(recordId)
+    // ownerUid: uid
+  };
+
+  const removeRecord = db.collection('records').remove(query);
+
+  await Promise.all([removeRecord]);
+
+  return true;
+};
+
 module.exports = {
   update_user,
-  get_user
+  get_user,
+  get_user_records,
+  get_user_forum_posts,
+  delete_record
 };
--- viny-site/lambda/user-management-service/user-main.js
+++ viny-site/lambda/user-management-service/user-main.js
@@ -38,8 +38,86 @@ exports.main = (event, context, callback) => {
         'POST',
         '/users',
         (event, context, callback) => {
-
-
+          user_functions.update_user(event.requestContext.authorizer.claims['sub'], event.body)
+            .then((user) => {
+              callback(null, lambdaRouter.builResponse(200, {
+                ...user,
+                success: true
+              }))
+            }).catch((e) => {
+            console.error(e);
+            callback(null, lambdaRouter.builResponse(500, {
+              records: "ERROR",
+              success: false
+            }))
+          });
         }
     );
+
+    /**
+     * get user records
+     */
+    router.route(
+        'GET',
+        '/users/records',
+        (event, context, callback) => {
+          user_functions.get_user_records(event.requestContext.authorizer.claims['sub'], event.queryStringParameters)
+            .then((user) => {
+              callback(null, lambdaRouter.builResponse(200, {
+                ...user,
+                success: true
+              }))
+            }).catch((e) => {
+            console.error(e);
+            callback(null, lambdaRouter.builResponse(500, {
+              records: "ERROR",
+              success: false
+            }))
+          });
+        }
+    );
+
+  /**
+   * get user forum posts
+   */
+  router.route(
+    'GET',
+    '/users/forum',
+    (event, context, callback) => {
+      user_functions.get_user_forum_posts(event.requestContext.authorizer.claims['sub'], event.queryStringParameters)
+        .then((user) => {
+          callback(null, lambdaRouter.builResponse(200, {
+            ...user,
+            success: true
+          }))
+        }).catch((e) => {
+        console.error(e);
+        callback(null, lambdaRouter.builResponse(500, {
+          records: "ERROR",
+          success: false
+        }))
+      });
+    }
+  );
+
+  /**
+   * delete record
+   */
+  router.route(
+    'DELETE',
+    '/users/records/{recordId}',
+    (event, context, callback) => {
+
+      user_functions.delete_record(event.requestContext.authorizer.claims['sub'], event.pathParameters.recordId).then((data) => {
+        callback(null, lambdaRouter.builResponse(200, {
+          success: true
+        }));
+      }).catch((e) => {
+        console.error(e)
+        callback(null, lambdaRouter.builResponse(500, {
+          success: false
+        }));
+      });
+    }
+  );
 };
